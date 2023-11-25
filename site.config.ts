d68fc93fdc349d297b144edf615ce91dfcf37f40
Nick <nickfriedrich@outlook.com>
2023-11-25 07:15:13 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Leagal Notice',
+      pageId: '4a0612a81f474efa82d4d269fad54e69'
+    },
+    {
+      title: 'Projects',
+      pageId: '5efb1dc9ca404e27b65b92240cf9c78c'
+    }
+  ]
 })
